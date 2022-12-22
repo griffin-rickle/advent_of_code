f094a659a2ef6cdd3a297502eb778c8953aba0c6
Griff <rickleg.93@gmail.com>
2022-12-22 17:22:36 -0500

    Generalized output to put needed number of consecutive chars in a variable

--- python/day6.py
+++ python/day6.py
@@ -1,11 +1,12 @@
 import util
+
+needed_consecutive_chars = 4
 data = util.get_day_input(6)[0]
 
 counter: int = 0
-curr_set = set(data[counter:counter+4])
-while len(curr_set) < 4:
-    print(curr_set)
+curr_set = set(data[counter:counter + needed_consecutive_chars])
+while len(curr_set) < needed_consecutive_chars:
     counter += 1
-    curr_set = set(data[counter:counter+4])
+    curr_set = set(data[counter:counter + needed_consecutive_chars])
 
-print(counter + 4)
+print(counter + needed_consecutive_chars)
