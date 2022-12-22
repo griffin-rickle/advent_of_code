88e8ec272a665927d7955b4139230e6f55f7fd88
Griff <rickleg.93@gmail.com>
2022-12-22 17:27:30 -0500

    Revealing unstripped to the entire world

--- python/util.py
+++ python/util.py
@@ -1,13 +1,21 @@
 import os
 
+def get_day_input_unstripped(day: int) -> list[str] :
+    filename = os.path.sep.join(['inputs', 'day' + str(day) + '.txt'])
+    return get_lines(filename, stripped=False)
+
 def get_day_input(day: int) -> list[str] :
     filename = os.path.sep.join(['inputs', 'day' + str(day) + '.txt'])
     return get_lines(filename)
 
 
-def get_lines(filename: str) -> list[str] :
+def get_lines(filename: str, stripped: bool=True) -> list[str] :
     with open(filename) as f:
-        lines = [line.strip() for line in f.readlines()]
+        lines = [line for line in f.readlines()]
+        if stripped is True:
+            lines = [line.strip() for line in lines]
+        else:
+            lines = [line.replace('\n', '') for line in lines]
 
     return lines
 
